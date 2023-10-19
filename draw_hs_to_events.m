117849d91bcc9ef02ec13c76fde7a057c94b1d1e
PeggyS <peggy.skelly@gmail.com>
2023-10-19 17:34:33 -0400

    revised interaction between vicon_insoles.mlapp & foot_markers.mlapp
    using https://www.mathworks.com/help/matlab/creating_guis/creating-multiwindow-apps-in-app-designer.html as a guide, sent and stored vicon_insoles app instance in foot_markers.mlapp

--- draw_hs_to_events.m
+++ draw_hs_to_events.m
@@ -2,7 +2,7 @@ function draw_hs_to_events(app)
 
 
 % define inv and uninv sides
-if strcmpi(app.side(1), 'r')
+if strcmpi(app.caller_app.side(1), 'r')
 	inv_side = 'r';
 	uninv_side = 'l';
 else
@@ -16,29 +16,29 @@ uninv_hs = [uninv_side 'hs'];
 uninv_to = [uninv_side 'to'];
 
 % inv hs
-for e_cnt = 1:length(app.event_struct.(inv_hs).times)
-	t = app.event_struct.(inv_hs).times(e_cnt);
+for e_cnt = 1:length(app.caller_app.event_struct.(inv_hs).times)
+	t = app.caller_app.event_struct.(inv_hs).times(e_cnt);
 
 	line(app.UIAxes_inv_heel, [t t], app.UIAxes_inv_heel.YLim, 'Color', 'k', 'LineWidth', 1)
 	line(app.UIAxes_inv_toe, [t t], app.UIAxes_inv_toe.YLim, 'Color', 'k', 'LineWidth', 1)
 end
 % inv to
-for e_cnt = 1:length(app.event_struct.(inv_to).times)
-	t = app.event_struct.(inv_to).times(e_cnt);
+for e_cnt = 1:length(app.caller_app.event_struct.(inv_to).times)
+	t = app.caller_app.event_struct.(inv_to).times(e_cnt);
 
 	line(app.UIAxes_inv_heel, [t t], app.UIAxes_inv_heel.YLim, 'Color', 'g', 'LineWidth', 1)
 	line(app.UIAxes_inv_toe, [t t], app.UIAxes_inv_toe.YLim, 'Color', 'g', 'LineWidth', 1)
 end
 % uninv hs
-for e_cnt = 1:length(app.event_struct.(uninv_hs).times)
-	t = app.event_struct.(uninv_hs).times(e_cnt);
+for e_cnt = 1:length(app.caller_app.event_struct.(uninv_hs).times)
+	t = app.caller_app.event_struct.(uninv_hs).times(e_cnt);
 
 	line(app.UIAxes_uninv_heel, [t t], app.UIAxes_uninv_heel.YLim, 'Color', 'k', 'LineWidth', 1)
 	line(app.UIAxes_uninv_toe, [t t], app.UIAxes_uninv_toe.YLim, 'Color', 'k', 'LineWidth', 1)
 end
 % uninv to
-for e_cnt = 1:length(app.event_struct.(uninv_to).times)
-	t = app.event_struct.(uninv_to).times(e_cnt);
+for e_cnt = 1:length(app.caller_app.event_struct.(uninv_to).times)
+	t = app.caller_app.event_struct.(uninv_to).times(e_cnt);
 
 	line(app.UIAxes_uninv_heel, [t t], app.UIAxes_uninv_heel.YLim, 'Color', 'g', 'LineWidth', 1)
 	line(app.UIAxes_uninv_toe, [t t], app.UIAxes_uninv_toe.YLim, 'Color', 'g', 'LineWidth', 1)
